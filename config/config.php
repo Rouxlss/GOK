7764314296bf2d2684a444616d7d2ca5b6f06e1c
Rouxlss <arnol.ortizjs@gmail.com>
2021-09-02 13:52:38 -0600

    db config

--- config/config.php
+++ config/config.php
@@ -5,9 +5,9 @@ define('IMG', 'http://localhost/GOK/public/assets/img');
 
 $bdconfig = array(
     'host' => '51.79.17.190',
-    'db' => 'difusalmo_db',
-    'user' => 'difusalmo_user',
-    'password' => '3I.p+2&Zq[3W'
+    'db' => 'transforma_gok',
+    'user' => 'transforma_user_gok',
+    'password' => '367WhLu7J#&i'
 );
 
 ?>
