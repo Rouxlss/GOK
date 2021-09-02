e863a16b5aea964147ebdc7cae586b543262e400
Rouxlss <arnol.ortizjs@gmail.com>
2021-09-02 13:13:38 -0600

    main terminado

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -2,7 +2,13 @@ home__btn = document.querySelector('.home__btn');
 home__menu = document.querySelector('.home__menu');
 
 home__form = document.querySelector('.home__form');
+form__bg = document.querySelector('.form__bg');
 home__join = document.querySelector('.home__join');
+form__close = document.querySelector('.form__close');
+
+video__close = document.querySelector('.video__close');
+video__video = document.querySelector('.video__video');
+home__video = document.querySelector('.home__video');
 
 window.addEventListener('click', function(e){   
 
@@ -12,14 +18,41 @@ window.addEventListener('click', function(e){
         home__menu.classList.add('d-none');
     }
 
+    if (home__join.contains(e.target)){
+        form__bg.classList.remove('d-none');
+    } else if (form__close.contains(e.target)){
+        form__bg.classList.add('d-none');
+    }
     
 });
 
-home__join.addEventListener('click', ()=> {
-    home__form.classList.remove('d-none');
+video__video.addEventListener('click', ()=> {
+    home__video.classList.add('video__play');
+    video__close.classList.remove('d-none');
 })
 
-home__form.addEventListener('click', ()=> {
-    home__form.classList.add('d-none');
+video__close.addEventListener('click', ()=> {
+    home__video.classList.remove('video__play');
+    video__close.classList.add('d-none');
 })
+
+
+let modes = document.querySelectorAll('.mode');
+let modes_locked = document.querySelectorAll('.lock');
+
+modes.forEach(mode => {
+    if(mode.classList.contains('lock')){
+        mode.innerHTML += `<img src="public/assets/img/candado.png" alt="" class="locked">`;
+    }
+});
+
+modes_locked.forEach(mode => {
+    console.log('a');
+    mode.addEventListener('click', ()=>{
+        mode.classList.add('no-selectable')
+        setTimeout(() => {
+            mode.classList.remove('no-selectable')
+        }, 500);
+    })
+});
 
--- src/main/main.view.php
+++ src/main/main.view.php
@@ -21,19 +21,81 @@
         <div class="home__join">
             <img src="<?= IMG ?>/boton-game-of.png" alt="">
         </div>
-        <div class="home__form d-none">
-            <img class="form__container" src="<?= IMG ?>/marco-form.png" alt="">
-            <!-- <img class="form__button" src="<?= IMG ?>/form-btn.png" alt=""> -->
+        <div class="form__bg d-none">
+            <div class="home__form">
+                <img class="form__close" src="<?= IMG ?>/close.png" alt="form__close">
+                <img class="form__container" src="<?= IMG ?>/marco-form.png" alt="form__container">
+                <div class="form__group">
+                    <input type="text" placeholder="Nombre y apellido" class="form__input input_text">
+                    <input type="email" placeholder="Correo eletronico" class="form__input input_text">
+                    <input type="password" placeholder="Contraseña" class="form__input input_text">
+                    <input type="number" placeholder="Edad" class="form__input input_text">
+                    <div class="form__options">
+                        <div class="form__control">
+                            <input type="checkbox" id="terminos" class="form__input inputtext">
+                            <label for="terminos">Aceptar terminos y condiciones</label>
+                        </div>
+                        <div class="form__control form__radios">
+                            <div class="radio">
+                                <label for="M">M</label>
+                                <input type="radio" id="M" name="genero" class="form__input">
+                            </div>
+                            <div class="radio">
+                                <label for="F">F</label>
+                                <input type="radio" id="F" name="genero" class="form__input">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <img class="form__button" src="<?= IMG ?>/form-btn.png" alt="">
+            </div>
         </div>
         <div class="home__video">
+            <img class="video__close d-none" src="<?= IMG ?>/close.png" alt="form__close">
             <img class="video__container" src="<?= IMG ?>/video-container.png" alt="">
             <img class="video__video" src="<?= IMG ?>/video-img.png" alt="">
         </div>
     </div>
 </div>
 
+<div class="modes">
+    <img src="<?= IMG ?>/second-bg.png" alt="" class="modes__bg">
+    <div class="modes__history">
+        <img src="<?= IMG ?>/history-mode-bg.png" alt="" class="history__bg">
+        <img src="<?= IMG ?>/h-mode-title.png" alt="" class="history__title">
+        <div class="modes-toplay">
+            <div class="mode unlocked">
+                <img src="<?= IMG ?>/recluta.png" alt="" class="mode__img">
+            </div>
+            <div class="mode lock">
+                <img src="<?= IMG ?>/guerrero.png" alt="" class="mode__img">
+            </div>
+            <div class="mode lock">
+                <img src="<?= IMG ?>/legionario.png" alt="" class="mode__img">
+            </div>
+        </div>
+        <img src="<?= IMG ?>/rangos-text.png" alt="" class="history__text">
+    </div>
+    <div class="modes__other">
+        <div class="other__space">
+            <img src="<?= IMG ?>/other.png" alt="other__bg" class="other__bg">
+            <img src="<?= IMG ?>/medal_icon.png" alt="other__icon" class="other__icon">
+            <img src="<?= IMG ?>/medallas.png" alt="other__text" class="other__text">
+        </div>
+        <div class="other__space">
+            <img src="<?= IMG ?>/other.png" alt="other__bg" class="other__bg">
+            <img src="<?= IMG ?>/mundo1.png" alt="other__icon" class="other__icon">
+            <img src="<?= IMG ?>/mundos.png" alt="other__text" class="other__text">
+        </div>
+    </div>
+    <div class="online">
+        <img src="<?= IMG ?>/reina-trono.png" alt="queen" class="queen">
+        <img src="<?= IMG ?>/batalla-en-linea.png" alt="batalla-en-linea" class="battle-text">
+    </div>
+</div>
+
 <?php
 
-    include './src/static/footer.view.php';
+include './src/static/footer.view.php';
 
 ?>
